e916644adf6f4d18d1a6526bc3826255d5daf283
eXCore <excoreky@gmail.com>
2021-02-21 21:17:53 +0200

    Lab 1 - TestApp done

--- src/com/excore/java_lab_1/test/TestApp.java
+++ src/com/excore/java_lab_1/test/TestApp.java
@@ -1,24 +1,35 @@
 package com.excore.java_lab_1.test;
 
+import com.excore.java_lab_1.model.Timber;
 import com.excore.java_lab_1.model.Wood;
+import com.excore.java_lab_1.store.ProductStore;
 import com.excore.java_lab_1.store.WoodDirectory;
 
 public class TestApp {
-    
+    private WoodDirectory wd = new WoodDirectory();
+    private ProductStore ps = new ProductStore();
+
+    private float calcWeight() {
+        float result = 0f;
+        for (Timber t :
+                ps.getArr()) {
+            result += t.weight();
+        }
+        return result;
+    }
+
+    private void startApp() {
+        ps.add(new Timber(wd.get(1), 5f, 0.5f, 0.4f));
+        ps.add(new Timber(wd.get(2), 10f, 0.5f, 0.4f));
+
+        System.out.println(wd);
+        System.out.println(ps);
+
+        System.out.printf("Загальна вага: %1.3f", calcWeight());
+    }
 
     public static void main(String[] args) {
-        WoodDirectory wd = new WoodDirectory();
-        System.out.println(wd.get(3));
-
-        Wood w = new Wood(4, "Дуб", 1f);
-
-        if (wd.add(w)) {
-            System.out.println(w);
-        }
-
-        if (!wd.add(w)) {
-            System.out.println(w + " вже існує");
-        }
-
+        TestApp app = new TestApp();
+        app.startApp();
     }
 }
